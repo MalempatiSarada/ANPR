c456436aeb58c1f8ea3017744642bebc6fc32908
Sarada <89697350+MalempatiSarada@users.noreply.github.com>
2023-05-30 16:11:44 +0530

    Add files via upload

---
+++ Anprsys.py
@@ -0,0 +1,410 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Importing libraries
+
+# In[1]:
+
+
+import os
+import tensorflow as tf
+import object_detection
+from object_detection.utils import config_util
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.builders import model_builder
+import cv2 
+import numpy as np
+from matplotlib import pyplot as plt
+#get_ipython().run_line_magic('matplotlib', 'inline')
+import easyocr
+import csv
+import uuid
+from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.utils import load_img
+from sklearn.preprocessing import LabelEncoder
+from keras.models import model_from_json
+
+
+# # paths
+
+# In[2]:
+
+
+CUSTOM_MODEL_NAME = 'my_ssd_mobnet'
+PRETRAINED_MODEL_NAME = 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8'
+TF_RECORD_SCRIPT_NAME = 'generate_tfrecord.py'
+LABEL_MAP_NAME = 'label_map.pbtxt'
+
+
+# In[3]:
+
+
+paths = {
+    'WORKSPACE_PATH': os.path.join('Tensorflow', 'workspace'),
+    'SCRIPTS_PATH': os.path.join('Tensorflow','scripts'),
+    'APIMODEL_PATH': os.path.join('Tensorflow','models'),
+    'ANNOTATION_PATH': os.path.join('Tensorflow', 'workspace','annotations'),
+    'IMAGE_PATH': os.path.join('Tensorflow', 'workspace','images'),
+    'MODEL_PATH': os.path.join('Tensorflow', 'workspace','models'),
+    'PRETRAINED_MODEL_PATH': os.path.join('Tensorflow', 'workspace','pre-trained-models'),
+    'CHECKPOINT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME), 
+    'OUTPUT_PATH': os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'export'), 
+    'TFJS_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfjsexport'), 
+    'TFLITE_PATH':os.path.join('Tensorflow', 'workspace','models',CUSTOM_MODEL_NAME, 'tfliteexport'), 
+    'PROTOC_PATH':os.path.join('Tensorflow','protoc')
+ }
+
+
+# In[4]:
+
+
+files = {
+    'PIPELINE_CONFIG':os.path.join('Tensorflow', 'workspace','models', CUSTOM_MODEL_NAME, 'pipeline.config'),
+    'TF_RECORD_SCRIPT': os.path.join(paths['SCRIPTS_PATH'], TF_RECORD_SCRIPT_NAME), 
+    'LABELMAP': os.path.join(paths['ANNOTATION_PATH'], LABEL_MAP_NAME)
+}
+
+
+# In[5]:
+
+
+labels = [{'name':'licence', 'id':1}]
+
+with open(files['LABELMAP'], 'w') as f:
+    for label in labels:
+        f.write('item { \n')
+        f.write('\tname:\'{}\'\n'.format(label['name']))
+        f.write('\tid:{}\n'.format(label['id']))
+        f.write('}\n')
+
+
+# In[6]:
+
+
+# Load pipeline config and build a detection model
+configs = config_util.get_configs_from_pipeline_file(files['PIPELINE_CONFIG'])
+detection_model = model_builder.build(model_config=configs['model'], is_training=False)
+
+# Restore checkpoint
+ckpt = tf.compat.v2.train.Checkpoint(model=detection_model)
+ckpt.restore(os.path.join(paths['CHECKPOINT_PATH'], 'ckpt-11')).expect_partial()
+
+@tf.function
+def detect_fn(image):
+    image, shapes = detection_model.preprocess(image)
+    prediction_dict = detection_model.predict(image, shapes)
+    detections = detection_model.postprocess(prediction_dict, shapes)
+    return detections
+
+
+# In[7]:
+
+
+category_index = label_map_util.create_category_index_from_labelmap(files['LABELMAP'])
+
+
+# In[8]:
+
+
+def ocr_it(image, detections, detection_threshold, region_threshold):
+    
+    # Scores, boxes and classes above threhold
+    scores = list(filter(lambda x: x> detection_threshold, detections['detection_scores']))
+    boxes = detections['detection_boxes'][:len(scores)]
+    classes = detections['detection_classes'][:len(scores)]
+    
+    # Full image dimensions
+    width = image.shape[1]
+    height = image.shape[0]
+    
+    # Apply ROI filtering and OCR
+    for idx, box in enumerate(boxes):
+        roi = box*[height, width, height, width]
+        region = image[int(roi[0]):int(roi[2]),int(roi[1]):int(roi[3])]
+        reader = easyocr.Reader(['en'])
+        ocr_result = reader.readtext(region)
+        
+        text = filter_text(region, ocr_result, region_threshold)
+        
+        plt.imshow(cv2.cvtColor(region, cv2.COLOR_BGR2RGB))
+        plt.show()
+        #print(text)
+        return text, region
+
+
+# In[9]:
+
+
+#if there is multiple texts on license plate
+def filter_text(region, ocr_result, region_threshold):
+    rectangle_size = region.shape[0]*region.shape[1]
+    
+    plate = [] 
+    for result in ocr_result:
+        length = np.sum(np.subtract(result[0][1], result[0][0]))
+        height = np.sum(np.subtract(result[0][2], result[0][1]))
+        
+        if length*height / rectangle_size > region_threshold:
+            plate.append(result[1])
+    return plate
+
+
+# In[10]:
+
+
+def find_contours(dimensions, img) :
+
+    # Find all contours in the image
+    cntrs, _ = cv2.findContours(img.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Retrieve potential dimensions
+    lower_width = dimensions[0]
+    upper_width = dimensions[1]
+    lower_height = dimensions[2]
+    upper_height = dimensions[3]
+    
+    # Check largest 5 or  15 contours for license plate or character respectively
+    
+    cntrs = sorted(cntrs, key=cv2.contourArea, reverse=True)[:15]
+    
+    ii = cv2.imread('contour.jpg')
+    
+    x_cntr_list = []
+    target_contours = []
+    img_res = []
+    for cntr in cntrs :
+        # detects contour in binary image and returns the coordinates of rectangle enclosing it
+        intX, intY, intWidth, intHeight = cv2.boundingRect(cntr)
+        
+        # checking the dimensions of the contour to filter out the characters by contour's size
+        if intWidth > lower_width and intWidth < upper_width and intHeight > lower_height and intHeight < upper_height :
+            x_cntr_list.append(intX) #stores the x coordinate of the character's contour, to used later for indexing the contours
+
+            char_copy = np.zeros((44,24))
+            # extracting each character using the enclosing rectangle's coordinates.
+            char = img[intY:intY+intHeight, intX:intX+intWidth]
+            char = cv2.resize(char, (20, 40))
+            
+            cv2.rectangle(ii, (intX,intY), (intWidth+intX, intY+intHeight), (50,21,200), 2)
+            plt.imshow(ii, cmap='gray')
+
+            # Make result formatted for classification: invert colors
+            char = cv2.subtract(255, char)
+
+            # Resize the image to 24x44 with black border
+            char_copy[2:42, 2:22] = char
+            char_copy[0:2, :] = 0
+            char_copy[:, 0:2] = 0
+            char_copy[42:44, :] = 0
+            char_copy[:, 22:24] = 0
+
+            img_res.append(char_copy) # List that stores the character's binary image (unsorted)
+            
+    # Return characters on ascending order with respect to the x-coordinate (most-left character first)
+            
+    #plt.show()
+    # arbitrary function that stores sorted list of character indeces
+    indices = sorted(range(len(x_cntr_list)), key=lambda k: x_cntr_list[k])
+    img_res_copy = []
+    for idx in indices:
+        img_res_copy.append(img_res[idx])# stores character images according to their index
+    img_res = np.array(img_res_copy)
+
+    return img_res
+
+
+# In[19]:
+
+
+# Find characters in the resulting images
+def segment_characters(image) :
+
+    # Preprocess cropped license plate image
+    img_lp = cv2.resize(image, (333, 75))
+    img_gray_lp = cv2.cvtColor(img_lp, cv2.COLOR_BGR2GRAY)
+    _, img_binary_lp = cv2.threshold(img_gray_lp, 200, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    img_binary_lp = cv2.erode(img_binary_lp, (3,3))
+    img_binary_lp = cv2.dilate(img_binary_lp, (3,3))
+
+    LP_WIDTH = img_binary_lp.shape[0]
+    LP_HEIGHT = img_binary_lp.shape[1]
+
+    # Make borders white
+    img_binary_lp[0:3,:] = 255
+    img_binary_lp[:,0:3] = 255
+    img_binary_lp[72:75,:] = 255
+    img_binary_lp[:,330:333] = 255
+    # Estimations of character contours sizes of cropped license plates
+    dimensions = [LP_WIDTH/6,
+                       LP_WIDTH/2,
+                       LP_HEIGHT/10,
+                       2*LP_HEIGHT/3]
+    plt.imshow(img_binary_lp, cmap='gray')
+    plt.show()
+    cv2.imwrite('contour.jpg',img_binary_lp)
+
+    # Get contours within cropped license plate
+    char_list = find_contours(dimensions, img_binary_lp)
+
+    return char_list
+
+
+# In[12]:
+
+
+#loading cnn model for reading text
+json_file = open('MobileNets_character_recognition.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = model_from_json(loaded_model_json)
+model.load_weights("License_character_recognition.h5")
+print("[INFO] Model loaded successfully...")
+
+labels = LabelEncoder()
+labels.classes_ = np.load('license_character_classes.npy')
+print("[INFO] Labels loaded successfully...")
+
+
+# In[14]:
+
+
+# model.summary()
+
+
+# In[13]:
+
+
+# Load the saved label file
+lb_classes = np.load('license_character_classes.npy')
+
+# Create a LabelEncoder and fit it with the loaded classes
+lb = LabelEncoder()
+lb.classes_ = lb_classes
+
+# pre-processing input images and predict with model
+def predict_from_model(image, model, lb):
+    image = cv2.resize(image, (80, 80))
+    image = np.stack((image,) * 3, axis=-1)
+    prediction = lb.inverse_transform([np.argmax(model.predict(image[np.newaxis, :]))])
+    return prediction
+
+
+# In[17]:
+
+
+def final_string(char):
+    final_string = ''
+    for i, character in enumerate(char):
+        title = np.array2string(predict_from_model(character, model, lb))
+        final_string += title.strip("'[]")
+    return final_string
+
+def save_results(text, region, csv_filename, folder_path):
+    img_name = '{}.jpg'.format(uuid.uuid1())
+    
+    cv2.imwrite(os.path.join(folder_path, img_name), region)
+    
+    with open(csv_filename, mode='a', newline='') as f:
+        csv_writer = csv.writer(f, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([img_name, text])
+
+# In[27]:
+
+
+img=cv2.imread('car1.jpg')
+plt.imshow(img)
+
+
+# In[21]:
+
+
+#Bringing all together
+img = cv2.imread('car1.jpg')
+image_np = np.array(img)
+
+input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+detections = detect_fn(input_tensor)
+
+num_detections = int(detections.pop('num_detections'))
+detections = {key: value[0, :num_detections].numpy()
+              for key, value in detections.items()}
+detections['num_detections'] = num_detections
+
+#detection_classes should be ints.
+detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+detection_threshold = 0.7
+region_threshold=0.6
+label_id_offset = 1
+image_np_with_detections = image_np.copy()
+text, region = ocr_it(image_np_with_detections, detections, detection_threshold, region_threshold)
+char = segment_characters(region)
+text = final_string(char)
+text 
+
+
+# In[15]:
+
+
+#For realtime feed
+cap = cv2.VideoCapture(0)
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+detection_threshold = 0.7
+region_threshold=0.6
+while cap.isOpened(): 
+    ret, frame = cap.read()
+    image_np = np.array(frame)    
+    input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.float32)
+    detections = detect_fn(input_tensor)
+    
+    num_detections = int(detections.pop('num_detections'))
+    detections = {key: value[0, :num_detections].numpy()
+                  for key, value in detections.items()}
+    detections['num_detections'] = num_detections
+
+    # detection_classes should be ints.
+    detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+    label_id_offset = 1
+    image_np_with_detections = image_np.copy()
+
+    viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections,
+                detections['detection_boxes'],
+                detections['detection_classes']+label_id_offset,
+                detections['detection_scores'],
+                category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=5,
+                min_score_thresh=.8,
+                agnostic_mode=False)
+    
+    try: 
+        text, region = ocr_it(image_np_with_detections, detections, detection_threshold, region_threshold)
+        chars = segment_characters(region)
+        texts = final_string(chars)
+        print(text)
+        save_results(texts, region, 'realtimeresults.csv', 'Detection_Images')
+    except:
+        pass
+
+    cv2.imshow('object detection',  cv2.resize(image_np_with_detections, (800, 600)))
+    
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        cap.release()
+        cv2.destroyAllWindows()
+        break
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
